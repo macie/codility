1bf31f0bfbff0ceff331a71fee4477c832597a17
macie <maciek.zok+github@gmail.com>
2014-06-11 20:25:08 +0200

    lesson 1: frog jumps

---
+++ Lessons/Lesson_1/FrogJmp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Problem:
+    Frog Jumps.
+
+In short:
+    Count minimal number of jumps from position X to Y.
+
+Result:
+    https://codility.com/demo/results/demoEU9S2X-PJ6/
+
+"""
+
+
+def solution(X, Y, D):
+    """
+    (distance // D)                 number of whole jumps
+    + (1 * ((distance % D) > 0))    if remainder of division then add 1
+
+    """
+    distance = Y - X
+    return (distance // D) + (1 * ((distance % D) > 0))
+
+
+if __name__ == "__main__":
+    X, Y, D = 10, 85, 30
+    print 'X =', X
+    print 'Y =', Y
+    print 'D =', D
+    print 'solution(X, Y, D) =', solution(X, Y, D)
