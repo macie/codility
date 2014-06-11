7f5bde6a4a72e85eb3a383cf2acad6b16b7231f2
macie <maciek.zok+github@gmail.com>
2014-06-11 20:53:11 +0200

    add PermMissingElem

---
+++ Lessons/Lesson_1/PermMissingElem.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Problem:
+    Missing element in permutation.
+
+In short:
+    Find the missing element in a given permutation.
+
+Result:
+    https://codility.com/demo/results/demoQXGA7N-75W/
+
+"""
+
+
+def solution(A):
+    """
+    Arithmetic series minus sum of A
+
+    """
+    N = len(A) + 1
+    sum_of_all = N * (1 + N)/2
+    for a in A:
+        sum_of_all -= a
+    return sum_of_all
+
+
+if __name__ == "__main__":
+    A = [2, 3, 1, 5]
+    print 'A =', A
+    print 'solution(A) =', solution(A)
